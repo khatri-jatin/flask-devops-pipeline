471b9261f3a383d7d55083f128e3abd04c2d93b2
khatri-jatin <107104104+khatri-jatin@users.noreply.github.com>
2025-07-12 15:25:58 +0530

    add the files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull jatin340/sample-flask-app:latest
+
+# Run the Docker image as a container
+docker -d -p 5000:5000 jatin340/sample-flask-app:latest
