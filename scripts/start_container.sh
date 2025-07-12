3e8b1cae357f0a27e0bf9e3f662f1a48a8b8458f
khatri-jatin <107104104+khatri-jatin@users.noreply.github.com>
2025-07-12 16:07:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-set -xe  # Print commands as they run, exit immediately on error
+set -e
 
-# Print Docker version
-sudo docker --version
-
-# Pull the Docker image from Docker Hub
 sudo docker pull jatin340/sample-flask-app:latest
-
-# Remove existing container if present
 sudo docker rm -f flask-app || true
-
-# Run the Docker container
 sudo docker run -d --name flask-app -p 5000:5000 jatin340/sample-flask-app:latest
