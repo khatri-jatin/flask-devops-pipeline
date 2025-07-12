7ba0e573e09acd38887e65eaa1484347db555fe0
khatri-jatin <107104104+khatri-jatin@users.noreply.github.com>
2025-07-12 14:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>code change </h1>"
+    return "<h1>hi simple python app </h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port= 5000 )
